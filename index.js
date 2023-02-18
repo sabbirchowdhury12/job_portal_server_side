98ce0796822a090d9e599b510ef5cddaf1ebc81f
sabbir <sabbir@gmail.com>
2023-02-18 15:21:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('hellow world');
+});
+app.listen(port, () => {
+    console.log('code is running');
+});
